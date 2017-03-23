b7b6b929015289fbfa11d2a12d69e474726b4b89
Nikolay Amiantov <ab@fmap.me>
2017-03-23 03:43:12 +0300

    Add simple database query

--- src/HomeModule.cs
+++ src/HomeModule.cs
@@ -1,12 +1,17 @@
 namespace funwithflags
 {
+    using System.Linq;
     using Nancy;
 
     public class HomeModule : NancyModule
     {
         public HomeModule(DatabaseContext database)
         {
-            Get("/", _ => "Hello Foo");
+            Get("/", _ =>
+                {
+                    var count = database.Tests.Count();
+                    return $"Hello! Test count: {count}";
+                });
             Get("/products/{id}", _ =>
             {
                 return "Hello Bar";

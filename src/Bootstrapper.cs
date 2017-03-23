cf1226e78b606704197c66d7ae85c48a089d727c
Nikolay Amiantov <ab@fmap.me>
2017-03-23 03:47:56 +0300

    Add verbose error reporting

--- src/Bootstrapper.cs
+++ src/Bootstrapper.cs
@@ -5,6 +5,7 @@ namespace funwithflags
     using Nancy;
     using Nancy.Bootstrapper;
     using Nancy.TinyIoc;
+    using Nancy.Configuration;
     
     public class CustomBootstrapper : DefaultNancyBootstrapper
     {
@@ -17,9 +18,19 @@ namespace funwithflags
                 .UseNpgsql(configuration["database"]);
             this.dbContextOptions = optionsBuilder.Options;
         }
+
+        public override void Configure(INancyEnvironment environment)
+        {
+            base.Configure(environment);
+            
+            // Enable verbose error reporting.
+            environment.Tracing(enabled: false, displayErrorTraces: true);
+        }
         
         protected override void ConfigureRequestContainer(TinyIoCContainer container, NancyContext context)
         {
+            base.ConfigureRequestContainer(container, context);
+
             // Create new database context and make it available.
             var dbcontext = new DatabaseContext(this.dbContextOptions);
             container.Register<DatabaseContext>(dbcontext);
